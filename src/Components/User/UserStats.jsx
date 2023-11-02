cfe45e51045087bef1c63a0ff4df999842ec02a1
viknupp <arq.vitoriaknupp@gmail.com>
2023-11-02 16:30:24 +0000

    add: external library

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import Head from "../Interface/Head";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Loading from "../Interface/Loading";
+import Error from "../Interface/Error";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
